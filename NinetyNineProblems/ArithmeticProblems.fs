474f1b9fab6280166a2e22f118e9cef2de38292f
danielgregory <dg334@outlook.com>
2015-05-05 22:56:07 +0100

    Added 2 solutions to the gcd problem and unit test.

--- NinetyNineProblems/ArithmeticProblems.fs
+++ NinetyNineProblems/ArithmeticProblems.fs
@@ -8,3 +8,18 @@ let isPrime n =
     let divisors = Seq.skipWhile(fun i -> n % i <> 0) integers
     if n = 0 then false else if n = 2 then true 
     else Seq.isEmpty divisors
+
+// 32 Find the greatest common divisor of two numbers using Euclid's algorithm.
+let rec gcd a b = 
+    let r = a % b
+    if r = 0 then b else gcd b r
+
+
+// TODO: why is this much faster than gcd?
+let gcd2 a b = 
+    let rec innergcd a b r = 
+        if r = 0 then b else innergcd b r (b % r)
+    innergcd a b (a % b)
+
+
+        
--- NinetyNineProblems/ArithmeticTests.fs
+++ NinetyNineProblems/ArithmeticTests.fs
@@ -13,3 +13,17 @@ type TestArithmeticProblems() =
         let notPrime = 25
         Assert.IsTrue (isPrime prime)
         Assert.IsFalse (isPrime notPrime)
+   
+    [<Test>]
+    member this.TestGcd() =
+        let a = 1071
+        let b = 462
+        Assert.AreEqual (21, gcd a b)
+
+    // interestingly gcd2 is much faster than gcd.
+    [<Test>]
+    member this.TestGcd2() =
+        let a = 1071
+        let b = 462
+        Assert.AreEqual (21, gcd2 a b)
+        
