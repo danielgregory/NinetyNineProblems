029ba469179ea04add7583945cf2c293fd9afa68
danielgregory <dg334@outlook.com>
2015-05-06 22:26:26 +0100

    Solutions to problems 33 and 34.

--- NinetyNineProblems/ArithmeticProblems.fs
+++ NinetyNineProblems/ArithmeticProblems.fs
@@ -14,12 +14,19 @@ let rec gcd a b =
     let r = a % b
     if r = 0 then b else gcd b r
 
-
 // TODO: why is this much faster than gcd?
 let gcd2 a b = 
     let rec innergcd a b r = 
         if r = 0 then b else innergcd b r (b % r)
     innergcd a b (a % b)
 
+// 33. Determine whether two positive integer numbers are coprime.
+// Two numbers are coprime if their greatest common divisor equals 1.
+let isCoPrime a b = 
+    (gcd2 a b) = 1
 
-        
+// 34. Euler's so-called totient function phi(m) is defined as the number
+//  of positive integers r (1 <= r <= m) that are coprime to m.
+let phi m =
+    let s = seq {for i in 1.. (m - 1) do if isCoPrime i m then yield i}
+    Seq.length s
